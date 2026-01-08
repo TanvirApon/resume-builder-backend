8c9bf4cb9f935256d6a8f3469080e869721fbb9e
tanvirhossainapon <tanvirhossainapon@gmail.com>
2026-01-08 22:48:11 +0600

    feat:Image Upload Code Added and Tested

--- resumebuilder/src/main/java/com/fullstack/resumebuilder/controller/AuthController.java
+++ resumebuilder/src/main/java/com/fullstack/resumebuilder/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.fullstack.resumebuilder.document.User;
 import com.fullstack.resumebuilder.dto.AuthResponse;
 import com.fullstack.resumebuilder.dto.RegisterRequest;
 import com.fullstack.resumebuilder.service.AuthService;
+import com.fullstack.resumebuilder.service.FileUploadService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
 
 import static com.fullstack.resumebuilder.util.AppContants.*;
@@ -24,6 +27,7 @@ import static com.fullstack.resumebuilder.util.AppContants.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final FileUploadService  fileUploadService;
 
     @PostMapping(REGISTER)
     public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) {
@@ -37,5 +41,11 @@ public class AuthController {
     public ResponseEntity<?> verifyEmail(@RequestParam  String token){
         authService.verifyEmail(token);
         return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","Email Verified Successfully"));
+    }
+
+    @PostMapping(UPLOAD_PROFILE)
+    public ResponseEntity<?> uploadImage(@RequestPart("image") MultipartFile file) throws IOException {
+        Map<String,String> response = fileUploadService.uploadSingleImage(file);
+        return ResponseEntity.ok(response);
     }
 }
--- resumebuilder/src/main/java/com/fullstack/resumebuilder/util/AppContants.java
+++ resumebuilder/src/main/java/com/fullstack/resumebuilder/util/AppContants.java
@@ -4,4 +4,5 @@ public class AppContants {
     public static final String AUTH_CONTROLLER = "/api/auth";
     public static final String REGISTER =  "/register";
     public static final String VERIFY_EMAIL = "/verifyEmail";
+    public static final String UPLOAD_PROFILE = "/upload-image";
 }
